ea95c6021d3b475d613c5455f13db9a3dcbb6ab7
Rashad Hamilton <rhamil12@lab.cpcc.edu>
2019-03-07 20:54:02 -0500

    20190307

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
 console.log("You are reading this from the console ");
+//document.getElementsByName("h1").style.color = "red";
+
+for(var i = 0; i<1000000000; i++);
+
+$("h1").css("color", "red");
+$("h1").innerHtml = "Changed from main.js";
+
+paper.install(window);
+//paper.setup(document.getElementById("mainCanvas"));
+paper.setup($("#mainCanvas")[0]);
+
+var tool = new Tool(); 
+
+tool.onMouseDown = function (event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = "green";
+
+}
+
+
+/*
+
+var c = Shape.Circle(200,200, 50);
+
+c.fillColor = 'green';
+*/
+
+
+paper.view.draw();
+
 });
